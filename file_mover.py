90c047b826c8bf218ac6081fb4b3ce28851566b1
Evan Forsythe <esfors@rams.colostate.edu>
2023-07-14 10:03:18 -0700

    added some test files for testing trinity's script

--- file_mover.py
+++ file_mover.py
@@ -18,20 +18,31 @@ import pandas as pd
 working_dir = sys.path[0]+'/' 
 os.chdir(working_dir)
 
+
+print(working_dir)
+
+
 #Set up an argument parser
 parser = argparse.ArgumentParser(description='Script for moving files based on a list input')
 
 parser.add_argument('-s', '--source_dir', type=int, metavar='', required=True, help='This should be a full path to the directory where the full pool of proteome files lives')
-parser.add_argument('-l','--list_file', type=int, metavar='', metavar=True, help='This should be a text file containing the name of each file to copy, with each file on a new line')
+parser.add_argument('-l','--list_file', type=int, metavar='', required=True, help='This should be a text file containing the name of each file to copy, with each file on a new line')
 parser.add_argument('-d','--dest_dir', type=str, metavar='', required=True, help='This should be a full path to the directory where you would like these to land; the scipt will create this folder if needed')
+
 
 #Define the parser
 args = parser.parse_args
+
 
 #Store arguments
 source_dir=args.source_dir
 list_file=args.list_file
 dest_dir=args.dest_dir
+
+print(source_dir)
+print(dest_dir)
+print(list_file)
+
 
 #Read in list file
 list_df=read_table(file=list_file)
